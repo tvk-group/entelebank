e5d5069b8a93b2fd085ae56efbcf1a520df4ee9b
TVK Group / TVK Labs & Technologies LTD – Ecosystem DevOps <team@tvklabs.com>
2026-08-15 15:30:21 +0300

    i18n: make language selector navigate safely

--- components/LanguageMenu.js
+++ components/LanguageMenu.js
@@ -1,7 +1,9 @@
+'use client';
 import { locales } from '../lib/i18n';
 
 export default function LanguageMenu({ current='en', path='' }) {
-  return <label className="languageMenu"><span className="srOnly">Language</span><select aria-label="Language" defaultValue={current} onChange={undefined}>
+  function changeLanguage(event){ window.location.assign(`/${event.target.value}${path}`); }
+  return <label className="languageMenu"><span className="srOnly">Language</span><select aria-label="Language" value={current} onChange={changeLanguage}>
     {locales.map(([code,name]) => <option key={code} value={code}>{name}</option>)}
-  </select><noscript><span>Language routes: {locales.map(([code,name],i)=><span key={code}> {i?'· ':''}<a href={`/${code}${path}`}>{name}</a></span>)}</span></noscript></label>;
+  </select></label>;
 }
